d0dd6c9be60421987fd6287b6fbd1f10ac65fcfd
Marinade <marinadegames@gmail.com>
2021-11-06 22:56:01 +0300

    Add Components. Refrest css style

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className='allPage'>
+      <Header />
+      <Technologies />
+      <MySolvesTasks />
     </div>
+    
   );
+}
+
+const Header = () =>{
+  return(
+    <div className='header'>
+      <div><h1>HELLO, samurai! </h1></div>
+      <div className='headerLinks'>
+        <a >Home </a>
+        <a>News </a>
+        <a>Messages </a>
+        <a>Feed </a>
+      </div>
+    </div>
+  )
+}
+
+const Technologies = () =>{
+  return(
+    <div>
+    <h2> Technologies: </h2>
+      <div className="">
+        <ul>
+          <li><a>HTML</a></li>
+          <li>CSS</li>
+          <li>JS</li>
+          <li>REACT</li>
+          <li>REDUX</li>
+        </ul>
+      </div>
+      </div>
+  )
+}
+
+const MySolvesTasks = () => {
+  return(
+    <div>
+      <h2> My solved tasks: </h2>
+      <div className="App">
+        <ul>
+          <li><a>Counter Numbers</a></li>
+          <li><a>HTML</a></li>
+          <li><a>HTML</a></li>
+          <li><a>HTML</a></li>
+          <li><a>HTML</a></li>
+
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App;
