7f377645f0f921a6dab657ae51a0251e16e4480b
Marinade <marinadegames@gmail.com>
2021-11-16 23:49:32 +0300

    update css styles | add little components (note + buttons note)

--- src/components/Page/Main/Posts/Posts.tsx
+++ src/components/Page/Main/Posts/Posts.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import s from './Posts.module.css'
+import Post from './Post/Post'
 
 const Posts = () => {
     return (
         <div className={s.myPosts}>
-            <div className={s.headerPosts}>My Posts:</div>
+            <div className={s.headerPosts}>Send post:</div>
             <textarea maxLength={100} placeholder='Напишите что-нибудь...'
                       className={s.textAreaSendPost}/>
-            <button className={s.btnSendPost}>Send</button>
+            <div className={s.rightFlex}>
+                <button className={s.btnSendPost}>Send</button>
+            </div>
+
+
+            <Post/>
         </div>
     )
 }
