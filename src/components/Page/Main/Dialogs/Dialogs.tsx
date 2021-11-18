6f440387c8ad13c9bc5b0095a9e5dffff20d0329
Marinade <marinadegames@gmail.com>
2021-11-18 23:51:04 +0300

    update Dialogs & Profile | refactor code

--- src/components/Page/Main/Dialogs/Dialogs.tsx
+++ src/components/Page/Main/Dialogs/Dialogs.tsx
@@ -2,44 +2,39 @@ import React from "react";
 import {NavLink} from "react-router-dom";
 import s from './Dialogs.module.css'
 
+const Dialog = (props: any) => {
+  return(
+      <NavLink to={'/dialogs/' + props.id} className={s.navLinkDialog}>
+          <div className={s.dialog}>
+              <div className={s.nameUser}>{props.userName}</div>
+          </div>
+      </NavLink>
+  )
+}
+
+const Message = (props: any) => {
+  return <div className={s.message}>{props.text}</div>
+}
 
 const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsList}>
 
-                <NavLink to='/dialogs/1' className={s.navLinkDialog}>
-                    <div className={s.dialog + ' ' + s.active}>
-                        <div className={s.nameUser}>Eugene</div>
-                    </div>
-                </NavLink>
-
-                <NavLink to='/dialogs/2'>
-                    <div className={s.dialog}>
-                        <div className={s.nameUser}>Elina</div>
-                    </div>
-                </NavLink>
-
-                <NavLink to='/dialogs/3'>
-                    <div className={s.dialog}>
-                        <div className={s.nameUser}>Jack</div>
-                    </div>
-                </NavLink>
-
-                <NavLink to='/dialogs/4'>
-                    <div className={s.dialog}>
-                        <div className={s.nameUser}>Vlad</div>
-                    </div>
-                </NavLink>
+                <Dialog id='1' userName='Eugene Pashkevich'/>
+                <Dialog id='2' userName='Elina Pashkevich'/>
+                <Dialog id='3' userName='Jack Black'/>
+                <Dialog id='4' userName='Vlad Topalov'/>
+                <Dialog id='5' userName='Your Mamasha '/>
 
             </div>
 
             <div className={s.messages}>
-                <div className={s.message}>Hi, how are you?</div>
-                <div className={s.message}>Hello, i'm fine, thanks</div>
-                <div className={s.message}>you ok?</div>
-                <div className={s.message}>Yes, i'm right</div>
-                <div className={s.message}>Good!</div>
+                <Message text='Hi, how are you?'/>
+                <Message text="Hello, i'm fine, thanks"/>
+                <Message text='you ok?'/>
+                <Message text="Yes, i'm right"/>
+                <Message text="Good!"/>
             </div>
         </div>
     )
--- src/components/Page/Main/Profile/Profile.tsx
+++ src/components/Page/Main/Profile/Profile.tsx
@@ -1,24 +1,12 @@
 import React from "react";
-
-import s from './Profile.module.css'
 import Posts from "./Posts/Posts";
-
-let avatarLogo = 'http://cdn.onlinewebfonts.com/svg/img_568656.png'
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
     return (
         <div>
-            <div className={s.profile}>
-                <img id={s.avatarLogo} src={avatarLogo}/>
-                <div className={s.profileData}>
-                    <div className={s.nameUser}>Eugene Pashkevich</div>
-                    <div className=''><b>Name of birth:</b> 17.02.1997</div>
-                    <div className=''><b>City:</b> Minsk</div>
-                    <div className=''><b>Country:</b> Belarus</div>
-                    <div className=''><b>Web-site:</b> <a href='#'>marinadegames.ru</a></div>
-                </div>
-            </div>
 
+            <ProfileInfo/>
             <Posts/>
 
         </div>
