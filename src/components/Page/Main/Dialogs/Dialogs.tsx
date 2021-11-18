f06731dd3c997ac99b23152888e6e488c5416a16
Marinade <marinadegames@gmail.com>
2021-11-18 23:00:15 +0300

    ADD PAGE Dialogs | add dialog layout

--- src/components/Page/Main/Dialogs/Dialogs.tsx
+++ src/components/Page/Main/Dialogs/Dialogs.tsx
@@ -1,11 +1,46 @@
 import React from "react";
-
+import {NavLink} from "react-router-dom";
+import s from './Dialogs.module.css'
 
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsList}>
+
+                <NavLink to='/dialogs/1' className={s.navLinkDialog}>
+                    <div className={s.dialog + ' ' + s.active}>
+                        <div className={s.nameUser}>Eugene</div>
+                    </div>
+                </NavLink>
+
+                <NavLink to='/dialogs/2'>
+                    <div className={s.dialog}>
+                        <div className={s.nameUser}>Elina</div>
+                    </div>
+                </NavLink>
+
+                <NavLink to='/dialogs/3'>
+                    <div className={s.dialog}>
+                        <div className={s.nameUser}>Jack</div>
+                    </div>
+                </NavLink>
+
+                <NavLink to='/dialogs/4'>
+                    <div className={s.dialog}>
+                        <div className={s.nameUser}>Vlad</div>
+                    </div>
+                </NavLink>
+
+            </div>
+
+            <div className={s.messages}>
+                <div className={s.message}>Hi, how are you?</div>
+                <div className={s.message}>Hello, i'm fine, thanks</div>
+                <div className={s.message}>you ok?</div>
+                <div className={s.message}>Yes, i'm right</div>
+                <div className={s.message}>Good!</div>
+            </div>
         </div>
     )
 }
