f0ab750864b3f8ece5084845aac6516a1b777270
Marinade <marinadegames@gmail.com>
2022-01-15 20:01:29 +0300

    fix

--- src/Components/Main/Profile/Profile.tsx
+++ src/Components/Main/Profile/Profile.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import s from './Profile.module.css'
 import {Posts} from "./Posts/Posts";
 import {BigHead} from "@bigheads/core";
-import {PostType} from "../../../Redux/tempStore";
+import {PostType} from "../../../Redux/state";
 
 // types
 type ProfilePropsType = {
